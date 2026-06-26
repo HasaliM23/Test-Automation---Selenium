ddbd061c8796f42950c2f7bc6495cf643cb3d24a
HasaliM23 <hasali2000pm@gmail.com>
2026-06-26 11:10:55 -0700

    Add to cart

---
+++ src/test/java/LambdaTestStoreTest.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LambdaTestStoreTest {
+    WebDriver driver;
+    WebDriverWait wait;
+
+@BeforeMethod
+    public void setup (){
+
+    driver =new ChromeDriver();
+    driver.get("https://ecommerce-playground.lambdatest.io/");
+    driver.manage().window().maximize();
+    wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+
+}
+
+@Test
+    public void verifyProductSearchAndAddToCart(){
+
+    WebElement searchInput = wait.until(ExpectedConditions.elementToBeClickable(By.name("search")));
+    searchInput.sendKeys("Phone");
+
+    WebElement search = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Search']")));
+    search.click();
+
+
+    WebElement cart = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//button[@title='Add to Cart'])[1]")));
+
+    org.openqa.selenium.JavascriptExecutor js = (org.openqa.selenium.JavascriptExecutor) driver;
+    js.executeScript("arguments[0].click();", cart);
+
+
+
+}
+
+}
